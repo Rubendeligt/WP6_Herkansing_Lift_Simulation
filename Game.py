65777c751058a682215f0c407b91a33c2cca713c
1083272-Ruben <1083272@hr.nl>
2026-02-27 23:03:31 +0100

    full screen

--- Game.py
+++ Game.py
@@ -4,8 +4,8 @@ import sys
 def main():
     pygame.init()
 
-    WIDTH, HEIGHT = 800, 600
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    WIDTH, HEIGHT = screen.get_size()
     pygame.display.set_caption("Lift Simulator")
 
     clock = pygame.time.Clock()
